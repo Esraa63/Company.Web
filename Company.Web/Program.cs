bbe67487e1cfbec5c0f83978c5c4c73a6b3da16d
esraa <esraa@DESKTOP-9RKGVE6>
2024-08-31 00:43:14 +0300

    CreationOfDev

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -4,6 +4,7 @@ namespace Company.Web
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("hello from Dev");
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
