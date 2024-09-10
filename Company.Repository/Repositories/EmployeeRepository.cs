ddb8c64e6c5209dbf0a53a57e01311190022cb78
esraa <esraa@DESKTOP-9RKGVE6>
2024-09-10 06:59:49 +0300

    Handel Using Entities

--- Company.Repository/Repositories/EmployeeRepository.cs
+++ Company.Repository/Repositories/EmployeeRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Company.Data.Contexts;
-using Company.Data.Entites;
+using Company.Data.Entitis;
 using Company.Repository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Company.Repository.Repositories
 {
@@ -29,5 +24,7 @@ namespace Company.Repository.Repositories
         {
             throw new NotImplementedException();
         }
+
+   
     }
 }
