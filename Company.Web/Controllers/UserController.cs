bc36ce1e1b12994e395e1e5e297c739119cbe974
esraa <esraa@DESKTOP-9RKGVE6>
2024-09-20 15:59:07 +0300

    UserController (Index)

---
+++ Company.Web/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Company.Data.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager) 
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index(string searchIndex)
+        {
+            List<ApplicationUser> users;
+            if(string.IsNullOrEmpty(searchIndex))
+                users = await _userManager.Users.ToListAsync() ;
+           else
+                users = await _userManager.Users.Where(user => user.NormalizedEmail.Trim()
+                .Contains(searchIndex.Trim().ToUpper())).ToListAsync();
+           
+            return View(users);
+        }
+    }
+}
